d1dddd33f16f20649b23ba5caa780a84ee20aa8b
BiggsBottor <jonatan.lledo@gmail.com>
2019-09-18 10:11:45 +0200

    BBDD corregidos ejercicios del 4 en adelante

--- Queries_USFlights.sql
+++ Queries_USFlights.sql
@@ -23,6 +23,16 @@ AVG(`ArrDelay`) AS "prom_arribades"
 FROM `flights`, `usairports` 
 WHERE Origin = iata 
 GROUP BY city, colYear, colMonth
+/* corregida query */
+SELECT a.city,`colYear`,`colMonth`, 
+AVG(`ArrDelay`) AS "prom_arribades" 
+FROM `flights` f
+LEFT JOIN `usairports` a
+ON f.Origin = a.IATA
+GROUP BY city, colYear, colMonth
+ORDER BY city, colYear, colMonth
+/* FIN corrección */
+
 
 /* 5. Les companyies amb més vols cancelats, per mesos i any. A més, han d’estar ordenades 
 de forma que les companyies amb més cancel·lacions apareguin les primeres. */
@@ -33,6 +43,15 @@ FROM `flights`
 GROUP BY `colYear`, `colMonth`, `UniqueCarrier` 
 ORDER BY `total_cancelled` DESC
 /* NOTE: es la versión más parecida a la mostrada al ejemplo */
+/* corregida query */
+SELECT `UniqueCarrier`, `colYear`, `colMonth`, 
+AVG(`ArrDelay`) AS "avg_delay", 
+SUM(`Cancelled`) AS "total_cancelled" 
+FROM `flights` 
+GROUP BY `colYear`, `colMonth`, `UniqueCarrier` 
+HAVING SUM(`Cancelled`) > 0
+ORDER BY `total_cancelled` DESC
+/* FIN corrección */
 
 /* 6. L’identificador dels 10 avions que més distància han recorregut fent vols. */ 
 SELECT `TailNum`, SUM(`Distance`) AS "total_distance" 
@@ -40,6 +59,14 @@ FROM `flights`
 WHERE `TailNum` <> "" 
 GROUP BY `TailNum` 
 ORDER BY `total_distance` DESC
+/* corregida query */
+SELECT `TailNum`, SUM(`Distance`) AS "total_distance" 
+FROM `flights` 
+WHERE `TailNum` <> "" 
+GROUP BY `TailNum` 
+ORDER BY `total_distance` DESC
+LIMIT 10
+/* FIN corrección */
 
 /* 7. Companyies amb el seu retard promig només d’aquelles les quals els seus vols 
 arriben al seu destí amb un retràs promig major de 10 minuts. */ 
@@ -49,3 +76,11 @@ FROM `flights`
 WHERE "avg_delay" < 10
 GROUP BY `UniqueCarrier`  
 ORDER BY `avg_delay`  DESC
+/* corregida query */
+SELECT `UniqueCarrier`,
+AVG(`ArrDelay`) AS "avg_delay"
+FROM `flights`
+GROUP BY `UniqueCarrier`  
+HAVING AVG(`ArrDelay`) > 10
+ORDER BY `avg_delay`  DESC
+/* FIN corrección */
